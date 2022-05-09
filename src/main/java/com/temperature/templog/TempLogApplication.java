88c214ddb894f1d8ed7efd6543a1508fe7e4a759
Svahnen <svahn.jonny@gmail.com>
2022-05-09 14:24:42 +0200

    Initial

---
+++ src/main/java/com/temperature/templog/TempLogApplication.java
@@ -0,0 +1,13 @@
+package com.temperature.templog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempLogApplication.class, args);
+	}
+
+}
